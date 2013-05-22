73df7d0e2ee78c7bbef30e656f97f3fa5b2b74f2
Andy Gill <andygill@ku.edu>
2013-05-22 11:08:09 -0500

    Better support for help

--- src/Main.hs
+++ src/Main.hs
@@ -33,22 +33,46 @@ data Env = Env
         }
 
 data Cmd = Add
-         | Dump
+         | Show
+         | Soon Int     -- in future, #n, n is days to go
+         | Mod Int      -- mod a specific record number
+         | Help
+         | Edit
+
 
 parseCmd :: TodoCmd -> (Cmd,TodoCmd)
-parseCmd (TodoCmd { description = [] }) = error "need mode: add | dump"
+parseCmd todoCmd@(TodoCmd { description = [] }) = (Show, todoCmd)
 parseCmd todoCmd@(TodoCmd { description = mode : args }) = (cmd, todoCmd { description = args })
   where cmd = case mode of
                 "add"  -> Add
-                "dump" -> Dump
+                "show" -> Show
+                "edit" -> Edit
+                "help" -> Help
+                "today" -> Soon 0
+                "tomorrow" -> Soon 1
+                ('+':ns) | all isDigit ns -> Soon (read ns)
+                ('#':ns) | all isDigit ns -> Mod (read ns)
 
 command :: Env -> TodoCmd -> Cmd -> IO ()
-command env cmds Dump = do
+command env cmds (Soon n) = command env cmds' Add
+  where cmds' = cmds { do_ = show n
+                     }
+
+command env cmds Help = do
+        putStrLn $ "modes: add | dump | today | +n | #n | edit"
+
+command env cmds Show = do
   let db = env_db env
-  putStrLn $ fullTitleLine
-  sequence_ [ putStrLn $ fullTaskLine env i t
+  let xs = [ fullTaskLine env i t
             | (i,t) <- Map.toList db
-            ]
+            , Prelude.null (description cmds) ||
+                show i `elem` (description cmds) ||
+                showNSWD (t_done t) `elem` (description cmds)
+           ]
+  if Prelude.null xs then return () else do
+          putStrLn $ fullTitleLine
+          putStr $ unlines xs
+
 command env cmds Add = do
    let today = env_today env
    td_by <- case readTaskDay today (by cmds) of
@@ -77,6 +101,8 @@ command env cmds Add = do
    writeDB (env_todo env) new_number task
    putStrLn $ fullTitleLine
    putStrLn $ fullTaskLine env new_number task
+command env cmds Edit = do
+        putStrLn $ "EDIT!"
 
 fullTitleLine = "    " ++ titleLine
 fullTaskLine env i t = rjust 3 ' ' (show i) ++ " " ++ taskLine (env_today env) t
