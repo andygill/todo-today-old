93b46ad02682f70da2db74715a0304b2459179ee
Andy Gill <andygill@ku.edu>
2013-05-22 11:07:23 -0500

    Ordering

--- src/Command.hs
+++ src/Command.hs
@@ -12,8 +12,8 @@ data TodoCmd = TodoCmd
                 { done          :: Maybe NSWD
                 , duration      :: String
                 , description   :: [String]     -- as parsed by shell
-                , by            :: String
                 , do_           :: String
+                , by            :: String
                 }
               deriving (Show, Data, Typeable)
 
@@ -21,8 +21,8 @@ todoCmd = TodoCmd
             { done        = def   &= help "is this task Needed, Wanted, Should be done, or Done"
             , duration    = def   &= help "duration of time expected to take to completion"
             , description = def   &= args &= typ "THE TODO"
-            , by          = def                 &= help "when this needs done by"
             , do_         = def   &= name "do"    &= help "when to do or review this"
+            , by          = def                   &= help "when this needs done by"
             }
 
 --          Add t -> Today t
